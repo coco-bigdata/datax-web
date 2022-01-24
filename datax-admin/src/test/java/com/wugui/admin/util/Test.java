76ff8ce7c194492a68d1b8d795b6da6642bd1f7e
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-01-24 13:39:56 +0800

    1. add docker

--- datax-admin/src/test/java/com/wugui/admin/util/Test.java
+++ datax-admin/src/test/java/com/wugui/admin/util/Test.java
@@ -1,7 +1,20 @@
 package com.wugui.admin.util;
 
 import com.wugui.datax.admin.util.AESUtil;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class Test {
 
     @org.junit.Test
